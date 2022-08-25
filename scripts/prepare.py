cc7e635276d818c279ff5f15f174b0b220042bf5
superior_god <yonglonggeng@163.com>
2022-08-25 22:25:42 +0800

    使用lunr进行构建索引

--- scripts/prepare.py
+++ scripts/prepare.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time:    2022-08-25 22:24
+# @Author:  geng
+# @Email:   yonglonggeng@163.com
+# @WeChat:  superior_god
+# @File:    prepare.py
+# @Project: rasa-3.x-action-examples
+# @Package:
+# @Ref:
+
 import typer
 import json
 from clumper import Clumper 
